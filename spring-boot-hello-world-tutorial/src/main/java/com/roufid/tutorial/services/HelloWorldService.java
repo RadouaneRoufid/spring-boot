c2c0a93af16724cc4faf4fbb7d83b6ebdb99353c
radouanerfd <radouaneroufid@gmail.com>
2016-12-15 19:52:35 +0100

    Initial commit

---
+++ spring-boot-hello-world-tutorial/src/main/java/com/roufid/tutorial/services/HelloWorldService.java
@@ -0,0 +1,12 @@
+package com.roufid.tutorial.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldService {
+
+	public String sayHelloWorld() {
+		return "Hello World !";
+	}
+
+}
